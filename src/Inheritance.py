fab2a6b21a2bffad510aabed898650fc9f7db87a
ragavan-web <ragavendhra21@gmail.com>
2026-02-08 10:51:47 +0530

    Inheritance

---
+++ src/Inheritance.py
@@ -0,0 +1,20 @@
+#29. Implement inheritance using `Employee` and `Manager` classes.
+class Employee:
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+    def display_employee(self):
+        print("Employee name is",self.name)
+        print("Salary:",self.salary)
+
+class Manager(Employee):
+    def __init__(self,name,salary,department):
+        super().__init__(name,salary)
+        self.department=department
+
+    def display_Manager(self):
+        print("Manager Dept:",self.department)
+
+m1=Manager("Mohan",30000,"IT")
+m1.display_employee()
+m1.display_Manager()
